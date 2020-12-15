81e80f43ec53a5baa1e02549f461029b44f2de03
thomasyu888 <thomas.yu@sagebase.org>
2020-12-15 15:09:24 +0800

    Fixg

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -4,18 +4,18 @@ import "fmt"
 
 func main() {
 
-	// Access Inputs as environment vars
-	firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
-	secondGreeting := os.Getenv("INPUT_SECONDGREETING")
-	thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+// Access Inputs as environment vars
+firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
+secondGreeting := os.Getenv("INPUT_SECONDGREETING")
+thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
 
-	// Use those inputs in the actions logic
-	fmt.Println("Hello " + firstGreeting)
-	fmt.Println("Hello " + secondGreeting)
+// Use those inputs in the actions logic
+fmt.Println("Hello " + firstGreeting)
+fmt.Println("Hello " + secondGreeting)
 
-	// Someimes inputs are not "required" and we can build around that
-	if thirdGreeting != "" {
-		fmt.Println("Hello " + thirdGreeting)
+// Someimes inputs are not "required" and we can build around that
+if thirdGreeting != "" {
+	fmt.Println("Hello " + thirdGreeting)
 	}
 
 }
